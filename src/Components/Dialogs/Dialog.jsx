2f451fd7cf6ed9b664d5755369179554bfeb86dd
CprMerkury <wsdsss@mail.ru>
2020-04-22 18:48:46 +0300

    table for test

--- src/Components/Dialogs/Dialog.jsx
+++ src/Components/Dialogs/Dialog.jsx
@@ -3,9 +3,23 @@ import s from './Dialog.module.css'
 
 const Dialog = (props) => {
     return (
-        <div>
-            <div>
-                dialogs
+        <div className='dialogs'>
+            <div className='dialogs-item'>
+                <div className='item'>
+                    Sveta
+                </div>
+                <div className='item'>
+                    Ivan
+                </div>
+                <div className='item'>
+                    Dima
+                </div>
+                <div className='item'>
+                    Max
+                </div>
+                <div className='item'>
+                    Vova
+                </div>
             </div>
         </div>
     )
