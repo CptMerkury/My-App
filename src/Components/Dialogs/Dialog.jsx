e9fa17093ab07b978b38949a0530afa114883212
CprMerkury <wsdsss@mail.ru>
2020-04-25 18:55:27 +0300

    table for test

--- src/Components/Dialogs/Dialog.jsx
+++ src/Components/Dialogs/Dialog.jsx
@@ -40,11 +40,17 @@ const Dialog = (props) => {
             <div className={s.dialogsItem}>
                 <DialogItem name={dialogData[0].name} id={dialogData[0].id}/>
                 <DialogItem name={dialogData[1].name} id={dialogData[1].id}/>
+                <DialogItem name={dialogData[2].name} id={dialogData[2].id}/>
+                <DialogItem name={dialogData[3].name} id={dialogData[3].id}/>
+                <DialogItem name={dialogData[4].name} id={dialogData[4].id}/>
+                <DialogItem name={dialogData[5].name} id={dialogData[5].id}/>
             </div>
             <div className={s.messages}>
                 <Message message={messageData[0].message}/>
                 <Message message={messageData[1].message}/>
-
+                <Message message={messageData[2].message}/>
+                <Message message={messageData[3].message}/>
+                <Message message={messageData[4].message}/>
             </div>
         </div>
     )
