3e592688e56d11960375e0ffc01496e388fd0051
CprMerkury <wsdsss@mail.ru>
2020-05-19 17:28:06 +0300

    Return File Test Git

---
+++ src/render.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import {addPost} from "./Redux/State";
+import {BrowserRouter} from "react-router-dom";
+import {updateNewPostText} from "./Redux/State";
+import {addMessage} from "./Redux/State";
+import {updateNewMessageText} from "./Redux/State";
+
+export let rerenderEntireTree = (state) => {
+    ReactDOM.render(
+        <React.StrictMode>
+            <BrowserRouter>
+                <App state={state}
+                     addPost={addPost}
+                     updateNewPostText={updateNewPostText}
+                     addMessage={addMessage}
+                     updateNewMessageText={updateNewMessageText}
+                />
+            </BrowserRouter>
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
