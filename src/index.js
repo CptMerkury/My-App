6c8e88f9f2e4ad718ed35e1388594312a0a3c4fd
maksimakubov <wsdsss@mail.ru>
2021-02-08 08:35:33 +0300

    minor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 import store from "./Redux/store";
 import './index.css';
 import App from './App';
@@ -24,3 +25,4 @@ store.subscribe(rerenderEntireTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
