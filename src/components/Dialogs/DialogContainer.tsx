8492d6160700c2792babb79d2e1e66828cf1c1ce
maksimakubov <wsdsss@mail.ru>
2021-04-20 20:42:34 +0300

    Typing app component

--- src/components/Dialogs/DialogContainer.tsx
+++ src/components/Dialogs/DialogContainer.tsx
@@ -5,6 +5,7 @@ import Dialog from "./DIalogLayout/Dialog";
 import withAuthRedirect from "../../utils/hoc/authRedirect";
 import {dialogActions} from "../../store/reducers/dialogs/dialogsReducer";
 import {AppStateType} from "../../store/store";
+import React from "react";
 
 const mapStateToProps = (state: AppStateType) => {
     return {
@@ -13,7 +14,7 @@ const mapStateToProps = (state: AppStateType) => {
     }
 }
 
-export default compose(
+export default compose<React.ComponentType>(
     withAuthRedirect,
     connect(mapStateToProps, {
         createMessage: dialogActions.sendMessageCreator
